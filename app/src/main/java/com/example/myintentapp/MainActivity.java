ecb84949e0e79f15c0ca82ef7e71cc4388ae22a5
rtninetyseven <taufiqurrahmantask@gmail.com>
2019-08-15 19:56:35 +0700

    Sampai nomor 5

--- app/src/main/java/com/example/myintentapp/MainActivity.java
+++ app/src/main/java/com/example/myintentapp/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.myintentapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnMoveActivity = findViewById(R.id.btn_move_activity);
+        btnMoveActivity.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_move_activity:
+                break;
+        }
     }
 }
