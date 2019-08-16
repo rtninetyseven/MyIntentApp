3f0957602a5b61213f9646cc63191d87db0d28bf
rtninetyseven <taufiqurrahmantask@gmail.com>
2019-08-16 21:50:28 +0700

    Intent Explicit Done On Proses Implicit

--- app/src/main/java/com/example/myintentapp/MainActivity.java
+++ app/src/main/java/com/example/myintentapp/MainActivity.java
@@ -27,6 +27,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 startActivity(moveIntent);
                 break;
             case R.id.btn_move_activity_data:
+                Intent moveWithDataIntent = new Intent(MainActivity.this, MoveWithDataActivity.class);
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_NAME, "DicodingAcademy Boy");
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_AGE, 5);
                 break;
         }
     }
